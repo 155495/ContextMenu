e6f620513d9203a2648091d0ec2dc4be83194bc1
Wozniak <bivinvinod512@gmail.com>
2018-02-26 22:32:15 +0530

    add another method on context menu

--- app/src/main/java/com/bivin/contextmenu/MainActivity.java
+++ app/src/main/java/com/bivin/contextmenu/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,19 +23,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        menu.setHeaderTitle("Select Any Option");
+
+        //Method 1
+        /*menu.setHeaderTitle("Select Any Option");
         menu.setHeaderIcon(R.mipmap.ic_launcher);
         menu.add("view More");
         menu.add("view less");
-        menu.add("view High");
-
+        menu.add("view High");*/
+//Method 2
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.context_menu,menu);
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-
+        //Method 1
+/*
         if(item.getTitle()=="view More"){
             Toast.makeText(getApplicationContext(),"View more",Toast.LENGTH_LONG).show();
+        }*/
+
+//Method 2
+        AdapterView.AdapterContextMenuInfo adapterContextMenuInfo= (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()){
+            case R.id.viewmore:
+                Toast.makeText(getApplicationContext(),"View Moere",Toast.LENGTH_LONG).show();
+                break;
         }
         return super.onContextItemSelected(item);
     }
